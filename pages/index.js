2d023a492bb8fbc7e90b8f4627c694f6ed9beff4
Luks3110 <lucas_bugado@hotmail.com>
2021-12-30 15:37:27 -0300

    Login page using Tailwind + NextJS + ReactJs

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,67 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { FaFacebook, FaGoogle, FaTwitter, FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"></link>
       </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          <div className="w-3/5 p-5">
+            <div className="text-left font-bold">
+              <span className="text-green-500">CompanyName</span>
+            </div>
+            <div className="py-10">
+              <h2 className="text-3xl font-bold text-green-500 mb-2">Sign in to your account</h2>
+              <div className="border-2 w-10 border-green-500 inline-block mb-2"></div>
+              <div className="flex justify-center my-2 ">
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                  <FaFacebook className="text-sm" />
+                </a>
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                  <FaGoogle className="text-sm" />
+                </a>
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                  <FaTwitter className="text-sm" />
+                </a>
+              </div> {/*Social login section*/}
+              <p className="text-gray-400 my-3">or use your email</p>
+              <div className="flex flex-col items-center ">
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                  <FaRegEnvelope className="text-gray-400 m-2" />
+                  <input type="email" name="email" placeholder="Email" className="bg-gray-100 outline-none text-sm
+                    flex-1"/>
+                </div>
+                <div className="bg-gray-100 w-64 p-2 flex items-center">
+                  <MdLockOutline className="text-gray-400 m-2" />
+                  <input type="password" name="password" placeholder="Password" className="bg-gray-100 outline-none text-sm
+                    flex-1"/>
+                </div>
+                <div className="flex justify-between w-64 mb-5">
+                  <label className="flex items-center text-xs my-1">
+                    <input type="checkbox" name="remember" className="mr-1" />
+                    Remember password
+                  </label>
+                  <a href="#" className="text-xs text-gray-500 ml-auto mt-1">Forgot password?</a>
+                </div>
+                <a href="#" className="border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"> Sign In</a>
+              </div>
+            </div>
+          </div>
+          {/*Sign in section*/}
+          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <h2 className="text-3xl font-bold mb-2"> Hello </h2>
+            <div className="border-2 w-10 inline-block mb-2"></div>
+            <p className="mb-10">Fill with your info and start the journey!</p>
+            <a href="#" className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"> Sign Up</a>
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
